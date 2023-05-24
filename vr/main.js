d25b71f2206ac27f1a090e6ec391a53100bb2117
thatkhay <johnkingsleyegeonu@gmail.com>
2023-05-24 14:14:21 +0100

    built a nav

--- vr/main.js
+++ vr/main.js
@@ -3,6 +3,8 @@
 
 const menuBtn = document.querySelector('.menu-btn');
 const hamburger = document.querySelector('.menu-btn_burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
 
 let showMenu = false;
 
@@ -11,9 +13,13 @@ menuBtn.addEventListener('click', toggleMenu);
 function toggleMenu() {
     if (!showMenu) {
         hamburger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
         showMenu = true;
     } else {
         hamburger.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
         showMenu = false;
     }
 }
